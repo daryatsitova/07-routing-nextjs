540baad67983773a84ae33fddfe53a2e97d08de5
Darya Tsitova <daryaatsitova@icloud.com>
2025-12-11 23:51:41 +0100

    Initial commit

---
+++ types/note.ts
@@ -0,0 +1,14 @@
+export interface Note {
+  title: string;
+  content: string;
+  tag: NoteTag;
+  id: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export type NoteTag = 'Todo' | 'Work' | 'Personal' | 'Meeting' | 'Shopping';
+
+export type NoteId = Note['id'];
+
+export type NotePost = Omit<Note, 'id' | 'createdAt' | 'updatedAt'>;
