d821bdc8715c2a867d04b1639fad4a1d5204b175
citauka <daryaatsitova@icloud.com>
2025-12-16 14:01:23 +0100

    sidebar default

--- app/notes/filter/@sidebar/default.tsx
+++ app/notes/filter/@sidebar/default.tsx
@@ -1,15 +1,22 @@
-import { getTags } from '@/lib/api';
 import css from './SidebarNotes.module.css';
 import Link from 'next/link';
 
+const tags: string[] = [
+  'All',
+  'Todo', 
+  'Work',
+  'Personal',
+  'Meeting',
+  'Shopping',
+];
+
 export default function SidebarNotes() {
-  const tags = getTags();
   return (
     <ul className={css.menuList}>
       {tags.map(tag => (
         <li key={tag} className={css.menuItem}>
           <Link href={`/notes/filter/${tag}`} className={css.menuLink}>
-            {tag}
+            {tag === 'All' ? 'All notes' : tag}
           </Link>
         </li>
       ))}
